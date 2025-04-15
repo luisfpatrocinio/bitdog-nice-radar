ec47316137fb89951218bf56299fe39c525896e8
Luis Felipe Patrocinio <patrocinioluisf@gmail.com>
2025-04-15 11:11:09 -0300

    feat: buzzer song implementation (panther theme)

--- libs/buzzer.c
+++ libs/buzzer.c
@@ -21,13 +21,16 @@ void beep(int duration)
 void playTone(uint frequency, uint duration_ms)
 {
     uint slice_num = pwm_gpio_to_slice_num(BUZZER_PIN);
-    uint32_t clock_freq = clock_get_hz(clk_sys);
-    uint32_t top = clock_freq / frequency - 1;
 
-    pwm_set_wrap(slice_num, top);
-    pwm_set_gpio_level(BUZZER_PIN, top / 2); // 50% de duty cycle
+    // Calcula o "top" (máximo valor de contagem do PWM)
+    float div = 16.0f; // Usar divisor 1.0 para maior resolução
+    uint32_t top = (uint32_t)(CLOCK_PWM / (frequency * div)) - 1;
+
+    pwm_set_clkdiv(slice_num, div);          // Configura divisor corretamente
+    pwm_set_wrap(slice_num, top);            // Define topo
+    pwm_set_gpio_level(BUZZER_PIN, top / 2); // 50% duty cycle
 
     sleep_ms(duration_ms);
 
-    pwm_set_gpio_level(BUZZER_PIN, 0); // Desliga o som após a duração
+    pwm_set_gpio_level(BUZZER_PIN, 0); // Para o som
 }
